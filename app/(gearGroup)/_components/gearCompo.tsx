69883fe244880f4fff0ae9d011b22cc523bf2a2b
TheCoderArif <mdarifulislam18539@gmail.com>
2026-08-08 13:06:18 +0600

    adding route for single gear

--- app/(gearGroup)/_components/gearCompo.tsx
+++ app/(gearGroup)/_components/gearCompo.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@/components/ui/card'
+import  Link  from 'next/link'
 import React from 'react'
 
 interface IGear {
@@ -25,15 +26,22 @@ const GearCompo = ({gear} : IGear) => {
     // console.log(gear);
   return (
     <div >
+       
+        <Link  href={`/gear/${gear.id}`}>
 
         <Card className=' my-2 px-3'>
             <img className='border-2 w-3/4 h-20 mx-auto pt-2 mt-2' src={gear.image} alt="image" />
         <p>Name: {gear.name}</p>
         <p>Description: {gear.description}</p>
-        <p>Price per day: {gear.pricePerDay}</p>
+        <p>Price per day: {gear.pricePerDay} $</p>
         <p>Availability: {gear.status}</p>
         <p>Quantity: {gear.quantity}</p>
         </Card>
+
+         
+            
+        </Link>
+        
 
     </div>
   )
--- app/(gearGroup)/gear/[id]/page.tsx
+++ app/(gearGroup)/gear/[id]/page.tsx
@@ -1,8 +1,38 @@
+import { Card } from '@/components/ui/card';
 import React from 'react'
 
-const page = () => {
+const page = async ({params}) => {
+
+  const {id} = await params;
+
+   const res = await fetch(
+    `${process.env.BACKEND_API_URL}/api/gear/${id}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch gear");
+  }
+
+  const gear = await res.json();
+
+  const data = gear.data.result;
+
+  // console.log(data);
   return (
-    <div>Gear Id</div>
+    <div>
+      <Card className=' my-2 px-3 max-w-3xl mx-auto'>
+        <div>{data.name}</div>
+        
+         <img className='border-2 w-3/4 h-20 mx-auto pt-2 mt-2' src={data.name} alt="image" />
+          <div className=''>Provider: {data.provider.name}</div>
+          <div>Category: {data.category.name}</div>
+          <div>Availability: {data.available}</div>
+          <div>Quantity: {data.quantity}</div>
+          <div>Available quantity: {data.available}</div>
+          <div>Price per day: {data.pricePerDay} $</div>
+
+      </Card>
+    </div>
   )
 }
 
